6525f85f62c6a7270a94d1a5e118ca233f25fcbf
Evin Ballantyne <EBallantyne@acceleratedsystems.com>
2024-04-17 15:39:20 -0400

    updates for clarity

--- PopoverCarouselBug/MainPage.xaml.cs
+++ PopoverCarouselBug/MainPage.xaml.cs
@@ -1,26 +1,46 @@
 ﻿using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Maui.Views;
+using System.Windows.Input;
 
-namespace PopoverCarouselBug
+namespace PopoverCarouselBug;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public static readonly BindableProperty CommandProperty = BindableProperty.Create("Command", typeof(ICommand), typeof(MainPage));
+
+    public ICommand Command
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            this.ShowPopup(new CustomPopup { Anchor = sender as View});
-        }
-
-        private void TouchBehavior_OnTouchGestureCompleted(object? sender, TouchGestureCompletedEventArgs e)
-        {
-            this.ShowPopup(new CustomPopup { Anchor = sender as View });
-        }
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
     }
 
+    public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create("CommandParameter", typeof(object), typeof(MainPage));
+
+    public object CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        Command = new Command(popupCommand);
+    }
+
+    private void popupCommand(object obj)
+    {
+        this.ShowPopup(new CustomPopup { Anchor = obj as View });
+    }
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        this.ShowPopup(new CustomPopup { Anchor = sender as View});
+    }
+
+    private void TouchBehavior_OnTouchGestureCompleted(object? sender, TouchGestureCompletedEventArgs e)
+    {
+        this.ShowPopup(new CustomPopup { Anchor = sender as View });
+    }
 }
